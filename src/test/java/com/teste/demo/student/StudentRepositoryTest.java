f367fa8c09d556f8acdec567711459a438499101
MariaMacedo-Prog <mariamacedo2xx@gmail.com>
2023-07-05 17:19:36 -0300

    Test if email exists or does not exists

--- src/test/java/com/teste/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/teste/demo/student/StudentRepositoryTest.java
@@ -1,36 +1,57 @@
 package com.teste.demo.student;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.mockito.ArgumentMatchers.assertArg;
 
 import java.time.LocalDate;
 import java.time.Month;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.teste.demo.student.StudentRepository;
+import com.teste.demo.student.Student;
+
+@DataJpaTest
 public class StudentRepositoryTest {
 
 	@Autowired
 	private StudentRepository underTest;
 	
 	@Test
-	void itShouldIfSelectExistsEmail() {
+	void itShouldIfSelectEmailExists() {
 		
-		//given
-		String email = "mariamacedo2xzasasx@gmail.com";
+		String email = "mariamacedo2xx@gmail.com";
+		//given 
 		Student student = new Student(
-				"maria",
-				email,
-				LocalDate.of(2000, Month.JANUARY, 25)
+					"maria",
+					email,
+					LocalDate.of(2000, Month.JANUARY, 25)
 				);
 		
-		underTest.save(student);
+    	underTest.save(student);
 		
 		//when
 		boolean exists = underTest.selectExistsEmail(email);
 		
 		//then
-		assertSame(exists,true);
+		assertEquals(exists, true);
+	}
+	
+	
+	@Test
+	void itShouldIfSelectEmailDoesNotExists() {
+		String email = "mariamacedoasssasasaa2xx@gmail.com";
+		
+		//given 
+		Student student = new Student(
+					"maria",
+					email,
+					LocalDate.of(2000, Month.JANUARY, 25)
+				);
+		//when
+		boolean exists = underTest.selectExistsEmail(email);
+		
+		//then
+		assertEquals(exists, false);
 	}
 }
